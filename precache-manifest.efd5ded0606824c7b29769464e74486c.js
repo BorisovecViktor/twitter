5dc00f73eedffbaf2a52adae68830d4f25f0db97
BorisovecViktor <viktorborisovets@gmail.com>
2020-08-31 16:58:56 +0300

    Updates

---
+++ precache-manifest.efd5ded0606824c7b29769464e74486c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80abd01321d4d962cfca3c028b7ddaed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34a05ce13504f48ea528",
+    "url": "./static/css/main.bc9b5dfc.chunk.css"
+  },
+  {
+    "revision": "8f1f5e38a7c89f10f231",
+    "url": "./static/js/2.d8881b3b.chunk.js"
+  },
+  {
+    "revision": "d82c31829d37991262b5e93968bcca1f",
+    "url": "./static/js/2.d8881b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a05ce13504f48ea528",
+    "url": "./static/js/main.b5e2f51f.chunk.js"
+  },
+  {
+    "revision": "8314e3554cb354590f57",
+    "url": "./static/js/runtime-main.05d1baf0.js"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "./static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "./static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/arrow-left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "6f553d93f2b3773340b890adab9ca954",
+    "url": "./static/media/close.6f553d93.svg"
+  },
+  {
+    "revision": "d5f941ebd8ca6134fbbbae1e936f6a34",
+    "url": "./static/media/sad-cry.d5f941eb.svg"
+  }
+]);
